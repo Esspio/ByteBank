c0f8f6fe31d059c8de800cf0a65217e10ab16fbf
Esspio <felipe.oliveira200085@gmail.com>
2024-05-29 20:01:18 -0300

    Teste para um metodo util

---
+++ src/utils/utils.test.js
@@ -0,0 +1,7 @@
+test("Deve retornar o valor do saldo atualizado com o rendimento", () => {
+    const calculaRendimento = jest.fn(saldo => saldo + saldo*0.005);
+    const saldo = 100;
+    const novoSaldo = calculaRendimento(saldo);
+
+    expect(novoSaldo).toBe(100.5);
+});
